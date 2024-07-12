90190311819cc38b850c3102b052120b42eb51a5
Akshat Sharma <akshatsharma779@gmail.com>
2024-07-13 03:30:51 +0530

    utils

---
+++ backendPlatform/src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHander = (func) => {
+    (req,res,next) => {
+        Promise.resolve(func(req,res,next)).catch((err) => next(err))
+        
+    }
+}
+
+export {asyncHander}
+
+
+// const asyncHandler = (func) => {async(req,res,next) => {
+//     try{
+//         await func(req,res,next)
+//     }
+//     catch(error){
+//         res.status(err.code||500).json({
+//             success:false,
+//             message: err.message
+//         })
+//     }
+
+// }}
