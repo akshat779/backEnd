dbc5833c4043cd1d7dc991525b851e7d05b2d343
Akshat Sharma <akshatsharma779@gmail.com>
2024-07-13 00:45:05 +0530

    database connect

--- backendPlatform/src/index.js
+++ backendPlatform/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/database.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path:"./env"
@@ -7,7 +8,15 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,() =>{
+        console.log("Server started at port 8000")
+    })
+})
+.catch((error) => {
+    console.log("Error occured in connecting to the DB ",error);
+})
 
 
 
